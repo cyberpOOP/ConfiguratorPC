d4ec516afd92b1cac2bede8bea4fdb6a34617266
double-hope <nadia.prohorchuk@gmail.com>
2023-02-15 14:47:16 +0200

    add input styles

--- frontend/src/components/primitives/search-input/component.tsx
+++ frontend/src/components/primitives/search-input/component.tsx
@@ -4,7 +4,6 @@ import * as styles from './styles';
 const SearchInput = () => {
 
   const [value, setValue] = useState('');
-  console.log(styles);
   
   return (
     <div css={styles.wrapper}>
--- frontend/src/components/primitives/search-input/styles.ts
+++ frontend/src/components/primitives/search-input/styles.ts
@@ -2,5 +2,9 @@ import { css } from '@emotion/react';
 import type { Theme } from '@emotion/react';
 
 export const wrapper = ({ colors }: Theme) => css`
-    width: 500px;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    max-width: 500px;
+    padding: 10px;
 `;
